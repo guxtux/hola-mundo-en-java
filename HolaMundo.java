5cb034172a1a05ecd1aba8e8a2e72f31983f20fe
Gustavo Panda <gus.contreras.m@gmail.com>
2018-05-31 14:42:51 -0500

    Se añadieron comentarios al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,9 @@
+/**
+ * Muestra en la pantalla la frase "¡Hola Mundo!"
+ *
+ *@author Gustavo
+ */
+
 public class HolaMundo{
 	public static void main(String[] args{
 		System.out.println("¡Hola Mundo!");	
