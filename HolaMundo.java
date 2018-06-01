c39f0bb9fbad2c54d01e09ebed2ff1af80def537
guxtux <gus.contreras.m@gmail.com>
2018-06-01 10:52:25 -0500

    Saludo en colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,7 +7,7 @@
 public class HolaMundo{
 	public static void main(String[] args) {
 		String naranja = "\033[33m";
-		String azul = "033[34m";
+		String azul = "\033[34m";
 		System.out.println(naranja + "¡Hola Mundo!");
 		System.out.println(azul + "Aquí estamos haciendo pruebas con Git.");
 	}
